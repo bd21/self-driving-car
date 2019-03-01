7bb213c237826da78d38e76bda52e4335a44f4f6
Collin.Feight <Collin.Feight@PACCAR.com>
2019-02-28 16:05:10 -0800

    yolo

--- project-2-advanced-lane-finding/Advanced_Lane_Finding.py
+++ project-2-advanced-lane-finding/Advanced_Lane_Finding.py
@@ -1,7 +1,8 @@
 import numpy as np
-import cv2
+import cv2 as cv
 import glob
 import matplotlib.pyplot as plt
+
 
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
 #objp defines number of horizonal white(9) and vertical black(6) intersections(corners) that occur
@@ -18,11 +19,11 @@ images = glob.glob('camera_cal/calibration*.jpg')
 
 # Step through the list and search for chessboard corners
 for fname in images:
-    img = cv2.imread(fname)
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    img = cv.imread(fname)
+    gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
 
     # Find the chessboard corners
-    ret, corners = cv2.findChessboardCorners(gray, (9,6),None)
+    ret, corners = cv.findChessboardCorners(gray, (9,6),None)
 
     # If found, add object points, image points
     if ret:
@@ -30,10 +31,14 @@ for fname in images:
         imgpoints.append(corners)
 
         # Draw and display the corners
-        img = cv2.drawChessboardCorners(img, (9,6), corners, ret)
-        cv2.imshow('img',img)
-        cv2.waitKey(500)
+        img = cv.drawChessboardCorners(img, (9,6), corners, ret)
+        #cv.imshow('img',img)
+        #cv.waitKey(500)
 
-cv2.destroyAllWindows()
+ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+distorted_image = cv.undistort(img, mtx, dist, None, mtx)
+print(mtx, dist)
+
+cv.imshow('unidstorted', distorted_image)
 
 #Next step is to find camera calibration matrix and distortion coeffs
