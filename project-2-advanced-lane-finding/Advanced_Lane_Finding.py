42bf85e6769638180e93fcade2a62e6df2acb3b9
blake <bd21@uw.edu>
2019-02-28 17:25:47 -0800

    it might be warping???

--- project-2-advanced-lane-finding/Advanced_Lane_Finding.py
+++ project-2-advanced-lane-finding/Advanced_Lane_Finding.py
@@ -3,11 +3,15 @@ import cv2 as cv
 import glob
 import matplotlib.pyplot as plt
 
+nx = 9
+ny = 6
+
+
 test = ('camera_cal/calibration1.jpg')
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
 #objp defines number of horizonal white(9) and vertical black(6) intersections(corners) that occur
-objp = np.zeros((6*9,3), np.float32)
-objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
+objp = np.zeros((ny*nx,3), np.float32)
+objp[:,:2] = np.mgrid[0:nx,0:ny].T.reshape(-1,2)
 
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d points in real world space
@@ -17,11 +21,47 @@ imgpoints = [] # 2d points in image plane.
 images = glob.glob('camera_cal/calibration*.jpg')
 #print(images)
 
-
-def UndistortImage(image, objectpoints, imagepoints):
+# make the lines straight
+def undistort_image(image, objectpoints, imagepoints):
     ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objectpoints, imagepoints, image.shape[1:], None, None)
     undistorted_image = cv.undistort(image, mtx, dist, None, mtx)
-    return undistorted_image
+    return undistorted_image, mtx, dist
+
+
+# fit the image according to the source points
+def warp_image(img, nx, ny, mtx, dist):
+
+    # convert to grayscale
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    ret, corners = cv.findChessboardCorners(gray, (nx, ny), None)
+
+    if ret == True:
+        # If we found corners, draw them! (just for fun)
+        cv.drawChessboardCorners(img, (nx, ny), corners, ret)
+
+        # Choose offset from image corners to plot detected corners
+        # This should be chosen to present the result at the proper aspect ratio
+        # offset
+        offset = 100
+        # Grab the image shape
+        img_size = (gray.shape[1], gray.shape[0])
+
+        # For source points I'm grabbing the outer four detected corners
+        src = np.float32([corners[0], corners[nx - 1], corners[-1], corners[-nx]])
+        # For destination points, I'm arbitrarily choosing some points to be
+        # a nice fit for displaying our warped result
+        # again, not exact, but close enough for our purposes
+        dst = np.float32([[offset, offset], [img_size[0] - offset, offset],
+                          [img_size[0] - offset, img_size[1] - offset],
+                          [offset, img_size[1] - offset]])
+        # Given src and dst points, calculate the perspective transform matrix
+        M = cv.getPerspectiveTransform(src, dst)
+        # Warp the image using OpenCV warpPerspective()
+        warped = cv.warpPerspective(img, M, img_size)
+
+
+        # Return the resulting image and matrix
+    return warped
 
 
 # Step through the list and search for chessboard corners
@@ -30,7 +70,7 @@ for fname in images:
     gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
 
     # Find the chessboard corners
-    ret, corners = cv.findChessboardCorners(gray, (9,6),None)
+    ret, corners = cv.findChessboardCorners(gray, (nx, ny),None)
 
     # If found, add object points, image points
     if ret:
@@ -39,13 +79,18 @@ for fname in images:
         imgpoints.append(corners)
 
         # Draw and display the corners
-        img = cv.drawChessboardCorners(img, (9,6), corners, ret)
+        # img = cv.drawChessboardCorners(img, (nx, ny), corners, ret)
         print("showing distorted image")
         cv.imshow('img', img)
-        cv.waitKey(2000)
+
         print("showing corrected image")
-        cv.imshow('img2', UndistortImage(img, objpoints, imgpoints))
-        cv.waitKey(2000)
+        corrected_image, mtx, dist = undistort_image(img, objpoints, imgpoints)
+        cv.imshow('img2', corrected_image)
+
+
+        print("showing unwarped image")
+        cv.imshow('img3', warp_image(corrected_image, nx, ny, mtx, dist))
+        cv.waitKey(4000) # wait 4 seconds
 
 
 
