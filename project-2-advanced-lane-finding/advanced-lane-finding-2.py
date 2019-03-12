5aaa6eb7e4de37be5f37447d2987ffa59d9be5e6
blake <bd21@uw.edu>
2019-03-12 02:05:39 -0700

    holy shit balls it's 2am and i'm just starting

---
+++ project-2-advanced-lane-finding/advanced-lane-finding-2.py
@@ -0,0 +1,51 @@
+import numpy as np
+import cv2
+import glob
+import matplotlib.pyplot as plt
+
+
+# The goal of this pipeline is to read in a video, frame by frame,
+# and output a video displaying the lane boundaries, lane curvature,
+# and vehicle position
+#
+# @author Blake Denniston (bd21)
+
+def main():
+    pipeline()
+
+
+def pipeline():
+    pass
+
+
+def calibrate_camera():
+    pass
+
+
+def transform_to_threshold_binary_image():
+    pass
+
+
+def transform_to_birds_eye_view():
+    pass
+
+
+def detect_and_fit_lane_lines():
+    pass
+
+
+def calculate_curvature():
+    pass
+
+
+def warp_boundaries_onto_frame():
+    pass
+
+
+def get_vehicle_position():
+    pass
+
+
+# so we can use functions before their declaration
+if __name__ == '__main__':
+    main()
